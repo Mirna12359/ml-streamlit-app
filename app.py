1a3add0f4db9eb9a7caae85daf483f6f192ac8a3
Merna Samy <81581798+Mirna12359@users.noreply.github.com>
2026-03-13 19:07:04 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open("model.pkl","rb"))
+
+st.title("House Price Prediction")
+
+size = st.number_input("Enter house size")
+
+if st.button("Predict"):
+    prediction = model.predict(np.array([[size]]))
+    st.success(f"Predicted price: {prediction[0]}")
