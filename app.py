41ed78a39f84c6b63118fd3d367de29eb383f748
Merna Samy <81581798+Mirna12359@users.noreply.github.com>
2026-03-13 19:19:41 +0200

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open("model.pkl","rb"))
+model = pickle.load(open("model.pkl", "rb"))
 
 st.title("House Price Prediction")
 
